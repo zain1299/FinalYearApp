22f20b656ca5068186960aeea1a28bd42378bdf1
zain1299 <zain.kiyani1999@gmail.com>
2022-02-04 01:32:41 +0500

    minor change in drawer

--- src/Components/Drawer.js
+++ src/Components/Drawer.js
@@ -12,7 +12,6 @@ import {
 } from "@react-navigation/drawer";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 const CustomDrawer = (props) => {
   return (
@@ -42,40 +41,14 @@ const CustomDrawer = (props) => {
               marginBottom: 5,
             }}
           >
-            John Doe
+            Zain
           </Text>
-          {/* <View style={{ flexDirection: "row" }}>
-            <Text
-              style={{
-                color: "#fff",
-                fontFamily: "Roboto-Regular",
-                marginRight: 5,
-              }}
-            >
-              280 Coins
-            </Text>
-            <FontAwesome5 name="coins" size={14} color="#fff" />
-          </View> */}
         </ImageBackground>
         <View style={{ flex: 1, backgroundColor: "#fff", paddingTop: 10 }}>
           <DrawerItemList {...props} />
         </View>
       </DrawerContentScrollView>
       <View style={{ padding: 20, borderTopWidth: 1, borderTopColor: "#ccc" }}>
-        {/* <TouchableOpacity onPress={() => {}} style={{ paddingVertical: 15 }}>
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Ionicons name="share-social-outline" size={22} />
-            <Text
-              style={{
-                fontSize: 15,
-                fontFamily: "Roboto-Medium",
-                marginLeft: 5,
-              }}
-            >
-              Tell a Friend
-            </Text>
-          </View>
-        </TouchableOpacity> */}
         <TouchableOpacity onPress={() => {}} style={{ paddingVertical: 15 }}>
           <View style={{ flexDirection: "row", alignItems: "center" }}>
             <Ionicons name="exit-outline" size={22} />
